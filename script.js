2ba5343133426507620b81098eef237bec84067e
Surbhi Jain <surbhijain.jain4@gmail.com>
2021-02-03 22:22:41 +0530

    Added sign up form

---
+++ script.js
@@ -0,0 +1,39 @@
+const form=document.getElementById('form');
+const username=document.getElementById('username')
+const Email=document.getElementById('Email')
+const password=document.getElementById('password')
+const password2=document.getElementById('confirm password')
+function showError(input,message){
+    const formControl=input.parentElement;
+    formControl.className='form-control error';
+    const small=formControl.querySelector('small');
+    small.innerText=message;
+}
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+//check email
+function isValidEmail(Email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+//check required fields
+function checkRequired(inputArr){
+    inputArr.forEach(function(input){
+        if(input.value>trim()===''){
+            showError(input,'is required')
+        }
+        else
+        {
+            showSuccess(input);
+        }
+    });
+}
+
+//event Listner
+form.addEventListener('submit', function (e){
+    e.preventDefault();
+
+checkRequired([username, Email, password, password2])
+});
